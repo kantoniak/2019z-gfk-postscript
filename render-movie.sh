470d5c966c526022b99c895b07093ea6db5bc53c
Krzysztof Antoniak <krzysztof@antoniak.in>
2019-10-20 14:57:26 +0200

    Change movie rendering to fixed frames

--- render-movie.sh
+++ render-movie.sh
@@ -8,4 +8,4 @@ mkdir -p "${OUTPUT_FOLDER}"
 
 # Render frames
 echo "Rendering movie..."
-ffmpeg -r:v ${FPS} -i "${PNG_FRAMES_FOLDER}/frame_%d.png" -tune animation -codec:v libx264 -preset veryslow -pix_fmt yuv420p -crf 28 -an "${OUTPUT_FOLDER}/movie.mp4"
+ffmpeg -framerate ${FPS} -i "${PNG_FRAMES_FOLDER}/frame_%d.png" "${OUTPUT_FOLDER}/movie.mp4"
